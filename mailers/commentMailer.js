50abd12e984f92add77b071a5fbe567f0349d29e
FalakChhikara <falakchhikara2001@gmail.com>
2021-01-21 05:30:53 +0530

    mailer done

--- mailers/commentMailer.js
+++ mailers/commentMailer.js
@@ -3,13 +3,18 @@ const password = require("../password");
 const nodemailer1 = require("nodemailer");
 
 exports.newCommentMail = async (comment) => {
+  let htmlString = nodemailer.renderTemplate(
+    { comment: comment },
+    "/comments/newComment.ejs"
+  );
+  // console.log("yyyyyyyyyyyyy", htmlString);
   nodemailer.transporter.sendMail(
     {
       from: password.EMAIL, // sender address
       to: comment.user.email, // list of receivers
       subject: "New Comment Published", // Subject line
       //   text: "Hello world?", // plain text body
-      html: "<b>New Comment Published</b>", // html body
+      html: htmlString, // html body
     },
     (err, info) => {
       if (err) {
